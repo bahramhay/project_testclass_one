c6f5b41a47ab0d3f13fd9f3ac6472f9a325ece79
bahram hay <bahramhayavi@yahoo.com>
2023-04-09 02:25:24 +0330

    some modifications

--- myapplication/input_dictionary.py
+++ myapplication/input_dictionary.py
@@ -3,13 +3,13 @@ import re
 materialsDict={}
 cookiesDict={}
 
-# numberOfMaterials=input("number of materials: \n")
-# for x in range(int(numberOfMaterials)):
-#     inputed = input("material: \n")
-#     words=inputed.split()
-#     if words[0]=="add":
-#         functions.adding_materials(materialsDict,words[1],words[2])
-# print(materialsDict)
+numberOfMaterials=input("number of materials: \n")
+for x in range(int(numberOfMaterials)):
+    inputed = input("material: \n")
+    words=inputed.split()
+    if words[0]=="add":
+        functions.adding_materials(materialsDict,words[1],words[2])
+print(materialsDict)
 
 numberOfCookies=input("number of cookies: \n")
 for x in range(int(numberOfCookies)):
